326718f70faef505f03abcb9a76b4e1c6cb261a3
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-04 16:37:00 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <main className="w-full min-h-screen bg-gradient-to-r from-fuchsia-500 to-cyan-500 flex items-center justify-center">
+            <h1 className="font-mono font-bold text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                Vite + React + Typescript + SWC + TailwindCSS + GitHub Pages
+            </h1>
+        </main>
+    );
+}
